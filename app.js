25ec5ad69986be91abeeaaa3da1a364acc453cce
Andrei Muntean <andrei.muntean.dev@gmail.com>
2017-03-31 20:51:48 +0300

    Added routes for messaging

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const 
+    bodyParser = require('body-parser'),
     config = require('config'),
+    crypto = require('crypto'),
     express = require('express'),
+    request = require('request');
 
 var app = express();
 app.set('port', process.env.HTTP_PLATFORM_PORT || 5000);
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
 
 // Setup config values before running the code. Can be done by modifying either config/default.json
 // or your operating system's environment variables.
@@ -17,15 +21,6 @@ if (!APP_SECRET) {
     process.exit(1);
 }
 
-const VALIDATION_TOKEN = process.env.HACKSOC_BOT_VALIDATION_TOKEN
-    ? process.env.HACKSOC_BOT_VALIDATION_TOKEN
-    : config.get('validationToken');
-
-if (!VALIDATION_TOKEN) {
-    console.error('Missing config value for validation token.');
-    process.exit(1);
-}
-
 const PAGE_ACCESS_TOKEN = process.env.HACKSOC_BOT_PAGE_ACCESS_TOKEN
     ? process.env.HACKSOC_BOT_PAGE_ACCESS_TOKEN
     : config.get('pageAccessToken');
@@ -35,11 +30,185 @@ if (!PAGE_ACCESS_TOKEN) {
     process.exit(1);
 }
 
+const VALIDATION_TOKEN = process.env.HACKSOC_BOT_VALIDATION_TOKEN
+    ? process.env.HACKSOC_BOT_VALIDATION_TOKEN
+    : config.get('validationToken');
+
+if (!VALIDATION_TOKEN) {
+    console.error('Missing config value for validation token.');
+    process.exit(1);
+}
+
 const SERVER_URL = process.env.HACKSOC_BOT_SERVER_URL
     ? process.env.HACKSOC_BOT_SERVER_URL
-    : config.get('serverUrl');
+    : config.get('serverURL');
 
 if (!SERVER_URL) {
     console.error('Missing config value for server URL.');
     process.exit(1);
 }
+
+app.get('/webhook', function(req, res) {
+    if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+        console.log('Webhook validated.');
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error('Failed validation. Make sure the validation tokens match.');
+        res.sendStatus(403);          
+    }  
+});
+
+app.post('/webhook', function(req, res) {
+    var data = req.body;
+
+    if (data.object == 'page') {
+        data.entry.forEach(function(pageEntry) {
+            var pageID = pageEntry.id;
+            var timeOfEvent = pageEntry.time;
+
+            // Iterate over each messaging event.
+            pageEntry.messaging.forEach(function(event) {
+                if (event.message) {
+                    receivedMessage(event);
+                } else if (event.delivery) {
+                    receivedDeliveryConfirmation(event);
+                } else if (event.read) {
+                    receivedMessageRead(event);
+                } else {
+                    console.log('Webhook received unknown messaging event:', event);
+                }
+            });
+        });
+
+        res.sendStatus(200);
+    }
+});
+
+/**
+ * Verifies that the callback came from Facebook. Read more at
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup.
+ */
+function verifyRequestSignature(req, res, buf) {
+    var signature = req.headers['x-hub-signature'];
+
+    if (!signature || signature.indexOf('=') === -1) {
+        throw new Error('Could not validate the request signature.');
+    }
+    
+    var elements = signature.split('=');
+    var signatureHash = elements[1];
+    var expectedHash = crypto
+        .createHmac('sha1', APP_SECRET)
+        .update(buf)
+        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+        throw new Error('Could not validate the request signature.');
+    }
+}
+
+/**
+ * This event is called when a message is sent to the bot. The 'message' object format can vary
+ * depending on the kind of message that was received. Read more at
+ * https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-received.
+ */
+function receivedMessage(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+
+    console.log('Received message for user %d and page %d at %d with message:',
+        senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+
+    var messageId = message.mid;
+    var appId = message.app_id;
+    var quickReply = message.quick_reply;
+
+    if (message.is_echo) {
+        console.log('Received echo for message %s and app %d with metadata "%s".',
+            messageId, appId, message.metadata);
+        return;
+    }
+
+    if (!message.quick_reply) {
+        console.log(
+            'Could not reply to message %s with metadata "%s".', messageId, message.metadata);
+        return;
+    }
+
+    sendQuickReply(senderID, payload);
+}
+
+/**
+ * This event is sent to confirm the delivery of a message. Read more about these fields at
+ * https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-delivered.
+ */
+function receivedDeliveryConfirmation(event) {
+    var delivery = event.delivery;
+    var messageIDs = delivery.mids;
+    var watermark = delivery.watermark;
+
+    if (messageIDs) {
+        messageIDs.forEach(function(messageID) {
+            console.log('Received delivery confirmation for message ID %d.', messageID);
+        });
+    }
+
+    console.log('All messages before %d were delivered.', watermark);
+}
+
+function sendQuickReply(recipientID, payload) {
+    var messageData = {
+        recipient: {
+            id: recipientID
+        },
+        message: {
+            text: 'Hello!',
+            quick_replies: [
+                {
+                    'content_type': 'text',
+                    'title': 'Hi',
+                    "payload": "HI"
+                },
+                {
+                    'content_type': 'text',
+                    'title': 'Hello',
+                    "payload": "HELLO"
+                }
+            ]
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+    request({
+        uri: 'https://graph.facebook.com/v2.8/me/messages',
+        qs: { access_token: PAGE_ACCESS_TOKEN },
+        method: 'POST',
+        json: messageData
+
+    }, function(error, response, body) {
+        if (error || response.statusCode != 200) {
+            console.error('Failed calling the send API:', response.statusCode, response.statusMessage, body.error);
+        }
+
+        var recipientId = body.recipient_id;
+        var messageId = body.message_id;
+
+        if (messageId) {
+            console.log('Successfully sent message with ID %s to recipient %s.', messageId, recipientId);
+        } else {
+            console.log('Successfully called the send API for recipient %s.', recipientId);
+        }
+    });
+}
+
+app.listen(app.get('port'), function() {
+    console.log('HackSoc bot is running on port %d.', app.get('port'));
+});
+
+module.exports = app;
